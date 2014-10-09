e2fd591254c3ab76c06c41f2296e1f8733653c39
clanrbr <clan_rbr@yahoo.com>
2014-10-09 15:10:13 +0300

    added Game of Thrones - I

---
+++ Java/Game of Thrones - I/Mainclass7.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// Game of Thrones - I
+public class Mainclass7 {
+
+    public static void solve(String myAnagram) {
+    	Hashtable<String, Integer> letters	= new Hashtable<String, Integer>();
+		char[] singleWord = myAnagram.toCharArray(); 
+		int sizeWord=singleWord.length;
+		for (int y=0;y<sizeWord;y++) {
+			Integer n = letters.get(String.valueOf(singleWord[y]));
+			if (n != null) {
+				letters.put(String.valueOf(singleWord[y]) ,n+1);
+			} else {
+				letters.put(String.valueOf(singleWord[y]) ,1);
+			}
+		}
+    		
+    	Set<Entry<String, Integer>> entrySet = letters.entrySet();
+    	Iterator<Entry<String, Integer>> it = entrySet.iterator();
+    	int result=0;
+    	int singleResult=0;
+    	while(it.hasNext()) {
+    		int newVal=letters.get(it.next().getKey());
+    		if (newVal==1) {
+    			singleResult++;
+    		} else {
+    			if ( newVal%2!=0 ) {
+        			result++;
+        		}
+    		}
+    	}
+    	
+    	String status="NO";
+    	
+    	if (sizeWord%2==0) {
+    		if ( (singleResult == 0) && (result==0) ) {
+    			status="YES";
+    		} else {
+    			status="NO";
+    		}
+    	} else {
+    		if ( singleResult==0 ) {
+    			if ( result==1 ) {
+    				status="YES";
+    			} else {
+    				status="NO";
+    			}
+    		} else {
+    			if ( (singleResult==1) && (result==0) ) {
+    				status="YES";
+    			} else {
+    				status="NO";
+    			}
+    		}
+    	}
+    	
+    	System.out.println(status);
+
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        String myAnagram =  in.next();
+        solve(myAnagram);
+    }
+}
