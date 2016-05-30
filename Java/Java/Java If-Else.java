627c44cf1ba71f24bbaf254d7a7651a1b2f52707
clanrbr <clan_rbr@yahoo.com>
2016-05-30 12:03:17 +0300

    added basics of java java if-else

---
+++ Java/Java/Java If-Else.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans="";
+        if (n%2!=0) {
+          ans = "Weird";
+        } else if ( (n%2==0) && (n>=2 && n<=5)){
+        	ans = "Not Weird";
+            
+        } else if ( (n%2==0) && (n>=6 && n<=20) ) {
+        	ans = "Weird";
+        } else if ( (n%2==0) && (n>20) ) {
+        	ans="Not Weird";
+        }
+        System.out.println(ans);
+        
+    }
+}
